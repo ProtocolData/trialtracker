1e9ed06e0ea7d4ed03d534e89f9d7557a098a17f
forrest.xiao <forrest.xiao@flatiron.com>
2022-08-21 03:13:24 -0500

    delete ctparser to re add submodule

--- src/trialtracker/extract/src/github.com/facebookresearch/Clinical-Trial-Parser/src/ct/variables/variable_id.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-
-package variables
-
-// ID defines the id of a variable.
-type ID string
-
-// Zero defines the zero id constant assigned to the ignored variable.
-const Zero = ID("0")
