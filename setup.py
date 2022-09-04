930a38062209d4bb0cf7eb3c17171dd2a8a036c1
forrest.xiao <forrest.xiao@flatiron.com>
2022-09-04 01:07:28 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
   name = 'trialtracker',         # How you named your package folder (MyLib)
   packages = ['trialtracker'],   # Chose the same as "name"
   package_dir={"":"src"},
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Methods to extract and transform clinical trial data',   # Give a short description about your library
   long_description=pypandoc.convert("README.md", "rst"),
